7ba7e7df567dc63ea4167e1516d74b0da16594c4
kavyansh18 <kavyanshkumar18@gmail.com>
2024-09-25 14:15:08 +0530

    feat:updateUI

--- src/App.jsx
+++ src/App.jsx
@@ -38,9 +38,9 @@ function App() {
 
   return (
     <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
-      <div className="bg-[#172842] min-h-screen py-3">
+      <div className="bg-black min-h-screen py-3">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 text-white">
-                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">When you will finish these hmmm?</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
                         <TodoForm />
